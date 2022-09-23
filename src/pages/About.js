35d29c3e7449a99d82a6f1cc80264822092f679e
Kyler Marquis <cryptojedi@MacBook-Pro-2.local>
2022-09-22 22:18:06 -0400

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About () {
+    return (
+    <>
+        <h1>About Page</h1>
+        <p>This page was uber obnoxious to built.</p>
+    </>
+    )
+}
+
+export default About;
